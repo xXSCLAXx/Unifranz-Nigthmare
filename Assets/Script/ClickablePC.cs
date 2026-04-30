53bb08f837b2edaeefed2ef149639b419b6a52cc
mew145 <yurijesus2005@gmail.com>
2026-04-30 16:28:33 -0400

    feat: initial commit - FNaF Panorama Unity project

---
+++ Assets/Script/ClickablePC.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class ClickablePC : MonoBehaviour
+{
+    public GameObject pcWindow;
+    public GameObject pasilloDerechaPanel;
+    public GameObject pasilloIzquierdaPanel;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouse = Input.mousePosition;
+            Debug.Log("Click: " + mouse);
+
+            // PC
+            if (mouse.x > 620 && mouse.x < 720 && mouse.y > 240 && mouse.y < 320)
+            {
+                AudioManager am = FindObjectOfType<AudioManager>();
+                if (am != null) am.PlayClickPC();
+                pcWindow.SetActive(true);
+            }
+
+            // Puerta derecha
+            if (!pasilloDerechaPanel.activeSelf && !pasilloIzquierdaPanel.activeSelf)
+            {
+                if (mouse.x > 1199 && mouse.x < 1344 && mouse.y > 61 && mouse.y < 631)
+                    pasilloDerechaPanel.SetActive(true);
+            }
+            else if (pasilloDerechaPanel.activeSelf)
+            {
+                if (mouse.x > 35 && mouse.x < 355 && mouse.y > 40 && mouse.y < 585)
+                    pasilloDerechaPanel.SetActive(false);
+            }
+
+            // Puerta izquierda
+            if (!pasilloIzquierdaPanel.activeSelf && !pasilloDerechaPanel.activeSelf)
+            {
+                if (mouse.x > 131 && mouse.x < 318 && mouse.y > 46 && mouse.y < 618)
+                    pasilloIzquierdaPanel.SetActive(true);
+            }
+            else if (pasilloIzquierdaPanel.activeSelf)
+            {
+                if (mouse.x > 131 && mouse.x < 318 && mouse.y > 46 && mouse.y < 618)
+                    pasilloIzquierdaPanel.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Script/LuzParpadeo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LuzParpadeo : MonoBehaviour
+{
+    public Image panelOscuro;
+    public float minTiempo = 0.05f;
+    public float maxTiempo = 0.2f;
+    public float minAlpha = 180f;
+    public float maxAlpha = 255f;
+
+    void OnEnable()
+    {
+        StartCoroutine(Parpadear());
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator Parpadear()
+    {
+        while (true)
+        {
+            // oscurece
+            float alpha = Random.Range(minAlpha, maxAlpha);
+            Color c = panelOscuro.color;
+            c.a = alpha / 255f;
+            panelOscuro.color = c;
+
+            yield return new WaitForSeconds(Random.Range(minTiempo, maxTiempo));
+
+            // aclara
+            c.a = 0f;
+            panelOscuro.color = c;
+
+            yield return new WaitForSeconds(Random.Range(minTiempo, maxTiempo));
+        }
+    }
+}
