00212be99770aa8c96a577d57fa99360d8ac27cc
Aldo Figueredo <matadorsalvatierra@gmail.com>
2026-06-08 09:27:18 -0400

    feat: add Unity project files with .gitignore

---
+++ Assets/Script/MenuPrincipal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Salir()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
